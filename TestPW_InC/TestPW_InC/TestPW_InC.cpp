45dd28233b29e3a546d8c63bea9399a8c7a5ce97
James Piper <github@jamespiper.com>
2017-01-12 08:34:24 -0500

    TestPW_InC
    Created code in VS2015

---
+++ TestPW_InC/TestPW_InC/TestPW_InC.cpp
@@ -0,0 +1,12 @@
+// TestPW_InC.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cstdio>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello World!");
+	return 0;
+}
+
